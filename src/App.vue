b96c178d40539c6f196778f0d6295455bb7b2404
mahsa karamloo <mahsa.k837@gmail.com>
2020-01-15 22:43:48 +0330

    challenge done

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,10 @@
   <div class="wrapper">
     <div class="header">
       <div class="logo">
-        <img src="./assets/images/logo.svg">
-
+        <img src="./assets/images/logo.svg" />
       </div>
       <div id="myNav" class="overlay">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="javascript:void(0)" class="closebtn" @click="closeNav()">&times;</a>
         <div class="overlay-content">
           <a href="#">Login</a>
           <a href="#">Pricing</a>
@@ -15,9 +14,7 @@
         </div>
       </div>
       <div class="menu">
-        <i class="material-icons" onclick="openNav()">
-          menu
-        </i>
+        <i class="material-icons" @click="openNav()">menu</i>
         <ul>
           <li>Product</li>
           <li>Features</li>
@@ -28,16 +25,22 @@
     </div>
     <div class="container">
       <div class="illus-img">
-        <img src="./assets/images/illustration-devices.svg">
+        <img src="./assets/images/illustration-devices.svg" />
       </div>
       <div class="text">
-        <div class="upper-title"><span>New</span>Monograph & nbsp Dashboard</div>
-        <div class="title">Powerful insights <br>into your team</div>
-        <div class="subtitle">Project planning and time tracking <br>for agile teams</div>
+        <div class="upper-title">
+          <span>New</span>Monograph & nbsp Dashboard
+        </div>
+        <div class="title">
+          Powerful insights
+          <br />into your team
+        </div>
+        <div class="subtitle">
+          Project planning and time tracking
+          <br />for agile teams
+        </div>
         <div class="btn">
-          <div class="btn-orange">
-            SCHEDULE A DEMO
-          </div>
+          <div class="btn-orange">SCHEDULE A DEMO</div>
           <div class="btn-white">TO SEE A PREVIEW</div>
         </div>
       </div>
@@ -47,4 +50,17 @@
 <style>
 @import url('./assets/style/index.css');
 </style>
+<script>
+export default {
+  methods: {
+    openNav() {
+      document.getElementById('myNav').style.width = '100%'
+    },
+
+    closeNav() {
+      document.getElementById('myNav').style.width = '0%'
+    }
+  }
+}
+</script>
 
